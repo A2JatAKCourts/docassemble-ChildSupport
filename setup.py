5fd8122a54848789880dac54c088eaf950742c11
Caroline Robinson <CRobinson@AKCourts.gov>
2025-03-12 07:42:55 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(name='docassemble.ChildSupport',
       url='https://docassemble.org',
       packages=find_packages(),
       namespace_packages=['docassemble'],
-      install_requires=['docassemble.AKA2JBranding>=25.3.7', 'docassemble.AssemblyLine>=3.2.0'],
+      install_requires=['docassemble.AssemblyLine>=3.2.0', 'docassemble-AKA2JBranding @ https://github.com/A2JatAKCourts/docassemble-AKA2JBranding/archive/main.zip'],
       zip_safe=False,
       package_data=find_package_data(where='docassemble/ChildSupport/', package='docassemble.ChildSupport'),
      )
